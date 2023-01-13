d0c6c563094d4792a5a8f851bc4e503c3030c4b0
unknown <bassel444555@gmail.com>
2023-01-13 19:19:41 +0200

    upload

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import cls from "./nav.module.css";
 import RequstBtn from "../RequstBtn";
 import Logo from "../Logo";
-
+import menue from "../../assets/icon-hamburger.svg";
+import close from "../../assets/icon-close.svg";
 export default function Nav() {
+  const [isActive, setActive] = useState(false);
+
+  const toggle = () => {
+    setActive(!isActive);
+  };
   return (
     <nav className={`${cls.nav} `}>
       <div className="container">
         <Logo color="#2D314D" />
-        <div className={cls.menu}>
+        <img
+          className={cls.burger}
+          src={isActive ? close : menue}
+          alt="icon"
+          onClick={toggle}
+        />
+        <div className={`${cls.menu} ${isActive ? cls.active : ""} `}>
           <ul>
             <li>
               <a href="#Home">Home</a>
